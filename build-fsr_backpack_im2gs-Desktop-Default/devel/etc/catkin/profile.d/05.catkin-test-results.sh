51f2a913ba4e13d4ee9e8f9d17c324604824ea4e
Jose Prado <xpecttrum@gmail.com>
2014-11-13 11:52:05 +0000

    removed directory one

--- build-fsr_backpack_im2gs-Desktop-Default/devel/etc/catkin/profile.d/05.catkin-test-results.sh
+++
@@ -1,4 +0,0 @@
-# generated from catkin/cmake/env-hooks/05.catkin-test-results.sh.develspace.in
-
-export CATKIN_TEST_RESULTS_DIR="/home/xpecttrum/catkin_ws/src/fsr_backpack/build-fsr_backpack_im2gs-Desktop-Default/test_results"
-export ROS_TEST_RESULTS_DIR="$CATKIN_TEST_RESULTS_DIR"
